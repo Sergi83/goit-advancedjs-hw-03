7da7b36ad7745083588eeb405b14f17a4d81b7e9
serhii_pmt <sergiy.pmt.20@gmail.com>
2023-11-18 22:15:38 +0200

    install axios, add key for api

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import axios
+import axios from 'axios';
+
+// add key to all headers
+axios.defaults.headers.common['x-api-key'] =
+  'live_SpY3U6UyCgy2bjzR7DbU0oLAhvNCZywFRPbwHdfvxbdcdP5NnP0R6vdYHdS1yx2N';
+
+//
